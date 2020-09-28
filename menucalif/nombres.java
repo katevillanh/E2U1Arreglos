952d843b2f233f919ce21b1f2f9642aba25e27a6
Kate Villanueva <71800218+katevillanh@users.noreply.github.com>
2020-09-27 23:20:41 -0500

    Create nombres.java

---
+++ menucalif/nombres.java
@@ -0,0 +1,48 @@
+
+package menucalif;
+
+public class nombres{
+    String[] datosUsuario=new String[15];
+    String[] datosCtrl=new String[15];
+    
+    public String [] metodoNom (String datosUsuario[]){
+        datosUsuario[0]="ACEVEDO CAMPOS ARIAGNA";
+        datosUsuario[1]="AMBROCIO PEREZ MICHAEL HAZIEL";
+        datosUsuario[2]="AMBROSIO HERNANDEZ VIANEY CELERINA";
+        datosUsuario[3]="CAMPOS CASASANERO JUAN PABLO";
+        datosUsuario[4]="DÍAZ ROSAS ALEXIS EFRÉN";
+        datosUsuario[5]="GOMEZ ARAGON EDGAR OMAR";
+        datosUsuario[6]="GUTIERREZ VALLE SAMARA";
+        datosUsuario[7]="JUAREZ RAMOS ALEXIS MANUEL";
+        datosUsuario[8]="LEYVA SILVA ROGELIO JESUS";
+        datosUsuario[9]="MARTINEZ DE LOS SANTOS ISABEL";
+        datosUsuario[10]="TAPIA SANCHEZ MARCO ANTONIO";
+        datosUsuario[11]="TATACOYA JULIAN";
+        datosUsuario[12]="VILLANUEVA AHUMADA KATE";
+        datosUsuario[13]="VILLANUEVA FLORES ERICK JARED";
+        datosUsuario[14]="ZAMORA ESPARZA YEZIER CRUZ";
+    return datosUsuario;   
+    }
+    
+    
+    
+    public String [] metodoCtrl (String datosCtrl[]){
+        datosCtrl[0]="18680056";
+        datosCtrl[1]="19680091";
+        datosCtrl[2]="19680092";
+        datosCtrl[3]="19680113";
+        datosCtrl[4]="17680136";
+        datosCtrl[5]="19680115";
+        datosCtrl[6]="19680162";
+        datosCtrl[7]="19680958";
+        datosCtrl[8]="19680177";
+        datosCtrl[9]="18680163";
+        datosCtrl[10]="18680754";
+        datosCtrl[11]="19680262";
+        datosCtrl[12]="17680246";
+        datosCtrl[13]="19680902";
+        datosCtrl[14]="19680277";
+        return datosCtrl;
+        
+    }
+}
